6f13f5143214b1973c7602f0a7d3c5608ac6a9cf
patorma <pocontreras@ing.ucsc.cl>
2025-02-06 00:13:22 -0300

    se modifico transportista y estudiante  para sus respectivos  controller

--- app/Http/Controllers/FurgonController.php
+++ app/Http/Controllers/FurgonController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\FurgonResource;
 use App\Models\Furgon;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -12,24 +13,42 @@ class FurgonController extends Controller
     public function addFurgon(Request $request){
         $validator = Validator::make($request->all(),[
            'patente' => 'required|string|min:8|max:8',
-            'descripcion' => 'required|string|min:10'
+            'descripcion' => 'required|string|min:10',
+             'usuario_id' => 'required|exists:users,id'
         ]);
 
         if($validator->fails()){
             return response()->json(['error'=> $validator->errors()],422);
         }
-
-           // Comprobamos si la patente ya existe
+      //  $usuario = User::find($request->get('usuario_id'));
+      //  $user = auth('api')->user();
+        $user_transportista= User::where('id',$request->get('usuario_id'))
+                                 ->where('role','transportista')
+                                 ->first();
+        if(!$user_transportista){
+            return response()->json(['error' => 'El usuario seleccionado no tiene el rol de transportista.'], 400);
+        }
+        //if($user->role === 'transportista'){
+                       // Comprobamos si la patente ya existe
            $existingFurgon = Furgon::where('patente', $request->get('patente'))->first();
            if ($existingFurgon) {
-               return response()->json(['error' => 'La patente ya existe en la base de datos.'], 400);
+                    return response()->json(['error' => 'La patente ya existe en la base de datos.'], 400);
            }
+      //  } else return response()->json(['error' => 'El usuario seleccionado no tiene el rol de transportista.'], 400);
 
-        $usuario_id = auth()->id();
+        // if (  !$usuario || !$user->role ==='transportista') {
+        //     return response()->json(['error' => 'El usuario seleccionado no tiene el rol de transportista.'], 400);
+        // }
+
+        //    if ($existingFurgon) {
+        //        return response()->json(['error' => 'La patente ya existe en la base de datos.'], 400);
+        //    }
+
+        //$usuario_id = auth()->id();
         $furgon =Furgon::create([
             'patente'=> $request->get('patente'),
             'descripcion' =>$request->get('descripcion'),
-            'usuario_id' => $usuario_id,
+            'usuario_id' => $request->get('usuario_id'),
         ]);
 
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
+    use HasRoles;
     /**
      * The attributes that are mass assignable.
      *
